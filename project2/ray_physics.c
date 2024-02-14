e92c33ca1f7f96cc67ccfbfdc864c5d0cb6431b0
CalebTalley2024 <calebtalley2024@gmail.com>
2024-02-14 01:44:39 +0000

    returned ray_physics back to normal (too many pthreads)

--- project2/ray_physics.c
+++ project2/ray_physics.c
@@ -1,4 +1,3 @@
-
 #include "ray_physics.h"
 #include "ray_math.h"
 
@@ -12,78 +11,14 @@ typedef int semaphore;
 
 // delta_velocity(int sphere_idx,struct context *ctx )  // producer
 
-pthread_mutex_t collision_mutex;
+
 // delta_position // consumer
 
-void *sphere_interaction(void *args){
 
-	struct Sphere_Int_Args *int_args = args; // interaction args
-	
-	// get pointers/values from args
-    sphere *si = int_args ->si; 
-    pt3 *pi = int_args ->pi;
-    pt3 *vi = int_args ->vi;
-    sphere *sj = int_args ->sj;
-    int i = int_args ->i;
-    int j = int_args ->j;
-
-
-	pt3 *pj = &sj->position;
-	pt3 *vj = &sj->velocity;
-	pt3 pdiff = pt3_sub(pi, pj);
-	pt3_normalize_mut(&pdiff);
-	pt3 vdiff = pt3_sub(vi, vj);
-
-	// if they're heading away from each other already, this is an unlucky frame
-	int approaching = pt3_dot(&vdiff, &pdiff) < 0;
-	// if (!approaching) continue;
-	if (!approaching){
-
-	pthread_mutex_lock(&collision_mutex);
-	// check for a collision (position distance less than sum of radii)
-	pt3 hit;
-	pt3 normal;
-	// if (!intersect_sphere_sphere(si, sj, &hit, &normal)) continue;
-	if (intersect_sphere_sphere(si, sj, &hit, &normal)){
-
-		// find projections of existing velocities onto the collision normal
-		// use the radii to find comparative masses. we assume uniform density for simplicity.
-		double ri = si->radius;
-		double rj = sj->radius;
-		double mi = 4.0 / 3.0 * M_PI * ri * ri * ri;
-		double mj = 4.0 / 3.0 * M_PI * rj * rj * rj;
-
-		double a = pt3_dotv(pt3_mul(&normal, 2), vdiff) / (1/mi + 1/mj);
-
-		if (0) fprintf(stderr, "collision pre  i %d j %d, vi %lf %lf %lf vj %lf %lf %lf ri %lf rj %lf mi %lf mj %lf a %lf\n", i, j,
-				vi->v[0], vi->v[1], vi->v[2],
-				vj->v[0], vj->v[1], vj->v[2],
-				ri, rj, mi, mj,
-				a);
-
-		*vi = pt3_addv(*vi, pt3_mul(&normal, -a / mi));
-		*vj = pt3_addv(*vj, pt3_mul(&normal, a / mj));
-
-		if (0) fprintf(stderr, "collision post i %d j %d, vi %lf %lf %lf vj %lf %lf %lf\n", i, j,
-				vi->v[0], vi->v[1], vi->v[2],
-				vj->v[0], vj->v[1], vj->v[2]);
-
-		}	
-	pthread_mutex_unlock(&collision_mutex);
-
-	}
-
-	return 0;
-}
-
-//slow
 int step_physics_velocity(struct context *ctx) {
 	// First we loop through all the spheres in the world, collision detect them with everything else,
 	// and modify the velocities _in place_. Note that velocities may be manipulated during the render
 	// phase as they don't affect position until they are applied.
-
-	pthread_t sphere_thread[ctx->num_spheres-1]; // create threads for spheres
-
 	for (int i = 0; i < ctx->num_spheres; i++) {
 		sphere *si = &ctx->spheres[i];
 		pt3 *pi = &si->position;
@@ -92,32 +27,43 @@ int step_physics_velocity(struct context *ctx) {
 		// sphere interaction
 		for (int j = i+1; j < ctx->num_spheres; j++) {
 			sphere *sj = &ctx->spheres[j];
+			pt3 *pj = &sj->position;
+			pt3 *vj = &sj->velocity;
+			pt3 pdiff = pt3_sub(pi, pj);
+			pt3_normalize_mut(&pdiff);
+			pt3 vdiff = pt3_sub(vi, vj);
 
-			// create args
-			struct Sphere_Int_Args *int_args = (struct Sphere_Int_Args*)malloc(sizeof(struct Sphere_Int_Args));
-			int_args->si = si;
-			int_args->pi = pi;
-			int_args->vi = vi;
-			int_args->sj = sj;
-			int_args->i = i;
-			int_args->j = j;
+			// if they're heading away from each other already, this is an unlucky frame
+			int approaching = pt3_dot(&vdiff, &pdiff) < 0;
+			if (!approaching) continue;
 
-			// sphere_interaction(si,pi,vi,sj,i,j);
-			pthread_create(&sphere_thread[j],NULL, sphere_interaction, int_args); // j-1 to use all threads
-			
-			// free(int_args);
-			
+			// check for a collision (position distance less than sum of radii)
+			pt3 hit;
+			pt3 normal;
+			if (!intersect_sphere_sphere(si, sj, &hit, &normal)) continue;
+
+			// find projections of existing velocities onto the collision normal
+			// use the radii to find comparative masses. we assume uniform density for simplicity.
+			double ri = si->radius;
+			double rj = sj->radius;
+			double mi = 4.0 / 3.0 * M_PI * ri * ri * ri;
+			double mj = 4.0 / 3.0 * M_PI * rj * rj * rj;
+
+			double a = pt3_dotv(pt3_mul(&normal, 2), vdiff) / (1/mi + 1/mj);
+
+			if (0) fprintf(stderr, "collision pre  i %d j %d, vi %lf %lf %lf vj %lf %lf %lf ri %lf rj %lf mi %lf mj %lf a %lf\n", i, j,
+					vi->v[0], vi->v[1], vi->v[2],
+					vj->v[0], vj->v[1], vj->v[2],
+					ri, rj, mi, mj,
+					a);
+
+			*vi = pt3_addv(*vi, pt3_mul(&normal, -a / mi));
+			*vj = pt3_addv(*vj, pt3_mul(&normal, a / mj));
+
+			if (0) fprintf(stderr, "collision post i %d j %d, vi %lf %lf %lf vj %lf %lf %lf\n", i, j,
+					vi->v[0], vi->v[1], vi->v[2],
+					vj->v[0], vj->v[1], vj->v[2]);
 		}
-
-		// this causes page fault
-
-		//for (int k = 0; k < ctx->num_spheres - 1; k++) { // Loop using the number of created threads
-		// 	int ret = pthread_join(sphere_thread[k], NULL); 
-		// 	if (ret != 0) {
-		// 		fprintf(stderr, "pthread_join failed with error code: %d\n", ret);
-		// 	} 
-		// }
-
 		
 		// plane interaction
 		for (int j = 0; j < ctx->num_planes; j++) {
@@ -138,7 +84,6 @@ int step_physics_velocity(struct context *ctx) {
 	return 0;
 }
 
-// fast, cannot be updated while raytracing
 int step_physics_position(struct context *ctx){
 	// Now that velocities are altered in the previous loop, apply the velocities (and gravity) to everything.
 	// This can't be run in parallel with rendering as it occurs now because the raytracer is dependent on the
--- project2/ray_physics.h
+++ project2/ray_physics.h
@@ -13,12 +13,4 @@ int step_physics_position(struct context *ctx);
 
 // custom
 
-struct Sphere_Int_Args{
-    sphere *si; 
-    pt3 *pi;
-    pt3 *vi; 
-    sphere *sj;
-    int i;
-    int j;
-};
 #endif	// RAY_PHYSICS_H__
