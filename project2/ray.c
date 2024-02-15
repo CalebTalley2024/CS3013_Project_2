1ba286509fae8426d52deba2fbcc8fd56fb6b779
CalebTalley2024 <calebtalley2024@gmail.com>
2024-02-15 03:12:58 +0000

    2.1 Finished(Concurrent physics and velocity)

--- project2/ray.c
+++ project2/ray.c
@@ -100,59 +100,44 @@ int main(int argc, char **argv)
 
 	sem_init(&velocity_updated, 0, 0); // velocity is consided to not be updated (0)
 	// init position_update to 1, (want velocity to start)
-	sem_init(&position_updated, 0, 2); // initial position considerd updated (1)
+	sem_init(&position_updated, 0, 1); // initial position considerd updated (1)
 
 	sem_init(&render_updated, 0, 0); // no initial render
 
 	// make render_args to put all arguements into renderThread
 	struct Render_Args *render_args = (struct Render_Args *)malloc(sizeof(struct Render_Args));
 
-	for (int frame = 0; frame < 4 * 25; frame++)
+	render_args->fb = fb;
+	render_args->ctx = ctx;
+	render_args->render_to_console = render_to_console;
+	// render_args->frame = frame;
+	render_args->frame = 0; // start frame at 0
+
+	render_args->argv = argv;
+
+	pthread_create(&render_thread, NULL, update_render, render_args); // thread for rendering
+	pthread_create(&velocity_thread, NULL, update_velocity, ctx);	  // (void*) type of function pointer
+
+
+
+	if (pthread_join(render_thread, NULL) != 0)
 	{
-		printf("\nINDEX: %d \n", frame);
-		// update render args
-		render_args->fb = fb;
-		render_args->ctx = ctx;
-		render_args->render_to_console = render_to_console;
-		render_args->frame = frame;
-		render_args->argv = argv;
-
-		
-		pthread_create(&render_thread, NULL, update_render, render_args); // thread for rendering
-		pthread_create(&velocity_thread, NULL, update_velocity, ctx); // (void*) type of function pointer
-
-		sem_wait(&render_updated);
-		sem_wait(&velocity_updated);
-
-		printf("position started\n");
-		step_physics_position(ctx);
-		printf("position finished\n");
-
-		sem_post(&position_updated);
-		sem_post(&position_updated);
-
-		// Join threads
-
+		printf("thread not working");
+		exit(-1);
 	}
-
-			if (pthread_join(render_thread, NULL) != 0)
-		{
-			printf("thread not working");
-			exit(-1);
-		}
-		else
-		{
-			// printf("\nrender thread works\n");
-		}
-		if (pthread_join(velocity_thread, NULL) != 0)
-		{
-			printf("thread not working");
-			exit(-1);
-		}
-		else
-		{
-			printf("\n velocity thread not working: %d\n", pthread_join(velocity_thread, NULL));
-		}
+	else
+	{
+		// printf("\nrender thread works\n");
+	}
+	if (pthread_join(velocity_thread, NULL) != 0)
+	{
+		printf("thread not working");
+		exit(-1);
+	}
+	else
+	{
+		printf("\n velocity thread not working: %d\n", pthread_join(velocity_thread, NULL));
+	}
 	// Destroy semaphores
 	sem_destroy(&velocity_updated);
 	sem_destroy(&render_updated);
@@ -174,48 +159,65 @@ void *update_render(void *args)
 	// printf("\nhe\n");
 	// @caleb arugments: struct framebuffer_pt4 * fb,struct context* ctx, int render_to_console,int frame,char **argv
 
-	
-	sem_wait(&position_updated);
-
 	// cast void pointer to render_arg pointer
 	struct Render_Args *render_args = args;
 	// printf("\nshe\n");
 	// get render args from render_args
 
-	render_scene(render_args->fb, render_args->ctx);
-
-	if (render_args->render_to_console)
+	for (int frame = 0; frame < 4 * 25; frame++)
 	{
-		render_console(render_args->fb);
-		usleep(50000);
-	}
-	else
-	{
-		char filepath[128];
-		// snprintf(filepath, sizeof(filepath) - 1, "%s-%05d.png", argv[2], frame);
-		// render_png(fb(physics), filepath);
-		snprintf(filepath, sizeof(filepath) - 1, "%s-%05d.bmp", render_args->argv[2], render_args->frame);
-		render_bmp(render_args->fb, filepath);
-	}
-	printf("Rendering started\n");
-	sem_post(&render_updated);
-	printf("Rendering finished\n");
 
+		
+		sem_wait(&position_updated);
+		printf("Rendering started\n");
+		render_scene(render_args->fb, render_args->ctx);
+
+		if (render_args->render_to_console)
+		{
+			render_console(render_args->fb);
+			usleep(50000);
+		}
+		else
+		{
+			char filepath[128];
+			// snprintf(filepath, sizeof(filepath) - 1, "%s-%05d.png", argv[2], frame);
+			// render_png(fb(physics), filepath);
+			snprintf(filepath, sizeof(filepath) - 1, "%s-%05d.bmp", render_args->argv[2], frame);
+			render_bmp(render_args->fb, filepath);
+		}
+		printf("Rendering finished\n");
+		sem_post(&render_updated);
+		
+	}
 	return 0;
 }
 
 void *update_velocity(void *_ctx)
 { // #TODO Make sure velocity goes before position
 
-	// sem_wait(&position_updated);
-	struct context *ctx = _ctx;
+	for (int frame = 0; frame < 4 * 25; frame++)
+	{
+		
+		printf("\nINDEX: %d \n", frame);
+		// no need for semaphores for velocity
+		printf("Velocity update started\n");
 
-	// no need for semaphores for velocity
-	printf("Velocity update started\n");
-	step_physics_velocity(ctx);
-	printf("Velocity update finished\n");
+		struct context *ctx = _ctx;
+		step_physics_velocity(ctx);
 
-	sem_post(&velocity_updated);
+		printf("Velocity update finished\n");
+		// sem_post(&velocity_updated);
+
+		sem_wait(&render_updated);
+		// sem_wait(&velocity_updated);
+
+		printf("position started\n");
+		step_physics_position(ctx);
+		printf("position finished\n");
+
+		sem_post(&position_updated);
+		// Join threads
+	}
 
 	return 0;
 }
